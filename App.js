d95983066519e491148e3a0b238b126f2072cc6f
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-29 16:11:03 -0700

    Updating routes and such

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import Expo from 'expo'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </TouchableOpacity>
       </View>
     );
   }
