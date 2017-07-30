6d7a80be0a01235cdf14d1f0f3b7c9c28e1e8f0b
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-07-29 17:56:10 -0700

    Fixed getUser route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -33,7 +33,7 @@ router.post('/getUser', function(req, res, next) {
         }
 
       })
-      res.json(user);
+      res.json(newUser);
     }
      else {
       res.json(user);
@@ -50,6 +50,8 @@ router.post('/getTenCards', function(req, res, next) {
     return cards
   });
   User.find({_id: userId}, function(err, user) {
+    // did not error catch here
+
     // should happen on newly registered user
     if(user.myCards.length === 0 && user.history.length === 0) {
       // shuffles cards and sends to front end
@@ -79,6 +81,7 @@ router.post('/getTenCards', function(req, res, next) {
 router.post('/vote', function(req, res, next) {
   var cardId = req.body.cardId;
   var userId = req.body.userId;
+
 })
 
 router.get('/uploadCard', function(req, res, next) {
