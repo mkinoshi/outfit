9fffe9d070a0b3e014842533ba33e7f9314b8e77
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-07-29 23:55:01 -0700

    midnight

--- server/env.sh
+++ server/env.sh
@@ -1,2 +1,5 @@
 export MONGODB_URI="mongodb://outfit:outfit@ds129023.mlab.com:29023/outfit"
 export SECRET="Send nerdz"
+
+export AWS_SECRET_KEY="hR3a1hoaNmJf71t/HA5Bgz4opkPdN/B7BFjR1716"
+export AWS_ACCESS_KEY="AKIAJ6TQHMYEL7WOGWZA"
